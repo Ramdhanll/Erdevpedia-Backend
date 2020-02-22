ab9b701aa74c856ff7eaab9e54ef636b131bf2d2
Ramdhanll <bscdhani30@gmail.com>
2020-02-22 14:12:12 +0700

    Create Model Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name','type','description','price','slug','quantity'
+    ];
+
+    protected $hidden = [
+    
+    ];
+
+    public function galleries(){
+        return $this->hasMany(ProductGallery::class,'product_id'); // ambilkan data di model ProductGallery yang fk product_id
+    }
 }
