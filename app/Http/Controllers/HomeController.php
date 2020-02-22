9ee8e87755b30c539db72110095739f0eb0f7239
Ramdhanll <bscdhani30@gmail.com>
2020-02-22 14:04:24 +0700

    Create Auth

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class DashboardController extends Controller
+class HomeController extends Controller
 {
     /**
      * Create a new controller instance.
@@ -16,9 +16,13 @@ class DashboardController extends Controller
         $this->middleware('auth');
     }
 
-    public function index(){
-        
-        return view('pages.dashboard');
-
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
     }
 }
