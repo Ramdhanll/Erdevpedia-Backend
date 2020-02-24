379085fe53747070811a134de6866ae79d321df9
Ramdhanll <bscdhani30@gmail.com>
2020-02-25 01:17:22 +0700

    Create Relasi Transaction Details

---
+++ app/Models/TransactionDetail.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TransactionDetail extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'products_id','transactions_id'
+    ];
+
+    protected $hidden = [
+    
+    ];
+
+    public function transaction() {
+        return $this->belongsTo(Transaction::class,'transaction_id','id');
+    }
+}
