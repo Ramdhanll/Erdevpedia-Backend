1c8faf550228964849250c6bd4d0bf281fde34aa
Ramdhanll <bscdhani30@gmail.com>
2020-02-24 13:37:47 +0700

    Create a Product Delete Page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -97,6 +97,8 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $item = Product::findOrFail($id);
+        $item->delete();
+        return redirect()->route('products.index');
     }
 }
